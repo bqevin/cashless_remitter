6cf851e8a825a70b7f14bc396734f81306ca76af
Kevin Barasa <kevin.barasa001@gmail.com>
2019-10-16 15:45:26 +0300

    (fix) : make email unique

--- priv/repo/migrations/20191014142211_create_users.exs
+++ priv/repo/migrations/20191014142211_create_users.exs
@@ -3,14 +3,15 @@ defmodule CashlessRemitter.Repo.Migrations.CreateUsers do
 
   def change do
     create table(:users) do
-      add :name, :string
-      add :email, :string
-      add :phone_number, :string
-      add :order_count, :integer
-      add :pin, :integer
+      add(:name, :string)
+      add(:email, :string)
+      add(:phone_number, :string)
+      add(:order_count, :integer)
+      add(:pin, :integer)
 
       timestamps()
     end
 
+    create(unique_index(:users, [:email], opts \\ []))
   end
 end
