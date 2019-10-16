602e7d1f64e6d4feed5c95ac81c476784d26e682
Kevin Barasa <kevin.barasa001@gmail.com>
2019-10-16 15:57:42 +0300

    (fix) : rename columns to make more sense

--- priv/repo/migrations/20191014142317_create_merchants.exs
+++ priv/repo/migrations/20191014142317_create_merchants.exs
@@ -3,15 +3,14 @@ defmodule CashlessRemitter.Repo.Migrations.CreateMerchants do
 
   def change do
     create table(:merchants) do
-      add :name, :string
-      add :logo, :string
-      add :description, :string
-      add :phone, :string
-      add :opening, :time
-      add :closing, :time
+      add(:name, :string)
+      add(:logo, :string)
+      add(:description, :string)
+      add(:phone, :string)
+      add(:opening_time, :time)
+      add(:closing_time, :time)
 
       timestamps()
     end
-
   end
 end
